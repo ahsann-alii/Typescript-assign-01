b99618873a3bf0870680d90b0c75fe7174676bda
ahsann-alii <ahsanzafar165@gmail.com>
2024-03-08 01:55:54 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+"use strict";
+let name = "Ahsan"; //this save the name
+console.log(`Hello ${name}, would you like to learn some coding today?`); //this shows the massage
---
+++ main.ts
@@ -0,0 +1,2 @@
+let name: string ="Ahsan"; //this save the name
+console.log(`Hello ${name}, would you like to learn some coding today?`); //this shows the massage
